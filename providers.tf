569a703717fc4122fd58da79a81da0bd44beb987
Manuel Patrone <po75558@gmail.com>
2025-01-28 19:01:47 +0000

    added github provider

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "6.5.0"
+    }
+  }
+}
+
+provider "github" {
+  # Configuration options
+  owner = "mpatrone"
+}
